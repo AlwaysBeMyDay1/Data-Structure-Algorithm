00e4fe4aea69be121b6a8ade6eeb5c319a9e3fd5
eodus0901 <>
2021-12-08 16:27:31 +0000

    b4 midterm

---
+++ 2021-2 Data-structure/LinkedList/HeadAddLinkedRead.c
@@ -0,0 +1,90 @@
+#if 0
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node
+{
+	int data;
+	struct _node * next;
+} Node;
+
+int main(void)
+{
+	Node * head = NULL;    // NULL � �ʱ�ȭ
+	Node * tail = NULL;
+	Node * cur = NULL;
+
+	Node * newNode = NULL;
+	int readData;
+
+	/**** �͸� �Է� �޴� � ****/
+	while(1)
+	{
+		printf("�ڿ� �Է�: ");
+		scanf("%d", &readData);
+		if(readData < 1)
+			break;
+
+		/*** � �߰� ***/
+		newNode = (Node*)malloc(sizeof(Node));
+		newNode->data = readData;
+		newNode->next = NULL;
+
+		if(head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			newNode->next = head;
+			head = newNode;
+		}
+	}
+	printf("\n");
+
+	/**** �Է� � � �°�****/
+	printf("�Է� � � �ü�! \n");
+	if(head == NULL) 
+	{
+		printf("� �ڿ� � �ʽ�ϴ�. \n");
+	}
+	else 
+	{
+		cur = head; 
+		printf("%d  ", cur->data);   // ù �° � �
+		
+		while(cur->next != NULL)    // � �° � � �
+		{
+			cur = cur->next;
+			printf("%d  ", cur->data);
+		}
+	}
+	printf("\n\n");
+
+	/**** �޸� � ***/
+	if(head == NULL) 
+	{
+		return 0;    // � �尡 � �ʴ´�.
+	}
+	else 
+	{
+		Node * delNode = head;
+		Node * delNextNode = head->next;
+
+		printf("%d�(�) �մϴ�. \n", head->data);
+		free(delNode);   // ù �° � �
+		
+		while(delNextNode != NULL)    // � �° � � � � �ݺ�
+		{
+			delNode = delNextNode;
+			delNextNode = delNextNode->next;
+
+			printf("%d�(�) �մϴ�. \n", delNode->data);
+			free(delNode);    // � �° � � �
+		}
+	}
+
+	return 0;
+}
+#endif
